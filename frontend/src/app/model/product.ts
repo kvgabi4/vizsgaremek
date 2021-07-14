7eae7ed54b723de481800ceefd9c63bb9a2df1aa
kvgabi4 <kvgabi4@gmail.com>
2021-07-14 15:04:08 +0200

    Product class

---
+++ frontend/src/app/model/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+  _id: string = '';
+  name: string = '';
+  category: string = '';
+  price: number = 0;
+  active: boolean = true;
+  image?: string = '';
+  description: string = '';
+}
