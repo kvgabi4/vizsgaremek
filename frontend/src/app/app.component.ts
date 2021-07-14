cd1e47b86ddfba8ddf1d3b194ff2638d41e6d57e
kvgabi4 <kvgabi4@gmail.com>
2021-07-14 22:15:45 +0200

    app.component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Bill } from './model/bill';
 import { Customer } from './model/customer';
+import { Order } from './model/order';
+import { Product } from './model/product';
 import { CustomerService } from './service/customer.service';
+import { ProductService } from './service/product.service';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +15,15 @@ import { CustomerService } from './service/customer.service';
 export class AppComponent {
   title = 'frontend';
 
+  products: Observable<Product[]> = this.productService.getAll();
   customers: Observable<Customer[]> = this.customerService.getAll();
+  orders: Observable<Order[]> = this.orderService.getAll();
+  bills: Observable<Bill[]> = this.billService.getAll();
 
   constructor(
+    private productService: ProductService,
     private customerService: CustomerService,
+    private orderService: OrderService,
+    private billService: BillService,
   ) {}
 }
