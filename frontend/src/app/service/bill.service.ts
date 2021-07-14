a67767be402677b57b67329babb058027f50fa2f
kvgabi4 <kvgabi4@gmail.com>
2021-07-14 22:18:56 +0200

    order, bill services

---
+++ frontend/src/app/service/bill.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Bill } from '../model/bill';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BillService extends BaseService<Bill> {
+
+  constructor(
+    public http: HttpClient,
+  ) {
+    super(http);
+    this.entity = 'customers';
+  }
+}
