d40a07b142efea4eeb9ffa7c904da520733bfd47
kvgabi4 <kvgabi4@gmail.com>
2021-07-14 15:34:13 +0200

    product service

---
+++ frontend/src/app/service/product.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../model/product';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService extends BaseService<Product> {
+  constructor(
+    public http: HttpClient,
+  ) {
+    super(http);
+    this.entity = 'products';
+  }
+}
