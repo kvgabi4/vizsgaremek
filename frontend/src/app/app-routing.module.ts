fd71c6b8007fa2de22638be45e9b17dff0f3678f
kvgabi4 <kvgabi4@gmail.com>
2021-07-20 12:09:05 +0200

    sb-admin template

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BillsComponent } from './page/bills/bills.component';
+import { CustomersComponent } from './page/customers/customers.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { OrdersComponent } from './page/orders/orders.component';
+import { ProductsComponent } from './page/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'customers',
+    component: CustomersComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+  {
+    path: 'orders',
+    component: OrdersComponent,
+  },
+  {
+    path: 'bills',
+    component: BillsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,24 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { BillsComponent } from './page/bills/bills.component';
+import { CustomersComponent } from './page/customers/customers.component';
+import { OrdersComponent } from './page/orders/orders.component';
+import { ProductsComponent } from './page/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+    BillsComponent,
+    CustomersComponent,
+    OrdersComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
