52aaed287d07da8bf533852c2325f429059eb476
kvgabi4 <kvgabi4@gmail.com>
2021-07-20 12:32:00 +0200

    config service

--- frontend/src/app/service/product.service.ts
+++ frontend/src/app/service/product.service.ts
@@ -2,15 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../model/product';
 import { BaseService } from './base.service';
+import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService extends BaseService<Product> {
   constructor(
+    public config: ConfigService,
     public http: HttpClient,
   ) {
-    super(http);
+    super(config, http);
     this.entity = 'products';
   }
 }
